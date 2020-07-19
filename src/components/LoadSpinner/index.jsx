7ca50f3691819f203b1a31cdb44f582bacd9761f
Sam M <62692646+seneka13@users.noreply.github.com>
2020-07-19 22:01:45 +0600

    change spinner

--- src/components/LoadSpinner/index.jsx
+++ src/components/LoadSpinner/index.jsx
@@ -4,11 +4,19 @@ import styles from './load.module.scss'
 function LoadSpinner() {
   return (
     <div className={styles.loadposit}>
-      <div className={styles.ldsEllipsis}>
-        <div />
-        <div />
-        <div />
-        <div />
+      <div className={styles.skCircle}>
+        <div className={`${styles.skCircle1} ${styles.skChild}`} />
+        <div className={`${styles.skCircle2} ${styles.skChild}`} />
+        <div className={`${styles.skCircle3} ${styles.skChild}`} />
+        <div className={`${styles.skCircle4} ${styles.skChild}`} />
+        <div className={`${styles.skCircle5} ${styles.skChild}`} />
+        <div className={`${styles.skCircle6} ${styles.skChild}`} />
+        <div className={`${styles.skCircle7} ${styles.skChild}`} />
+        <div className={`${styles.skCircle8} ${styles.skChild}`} />
+        <div className={`${styles.skCircle9} ${styles.skChild}`} />
+        <div className={`${styles.skCircle10} ${styles.skChild}`} />
+        <div className={`${styles.skCircle11} ${styles.skChild}`} />
+        <div className={`${styles.skCircle12} ${styles.skChild}`} />
       </div>
     </div>
   )
