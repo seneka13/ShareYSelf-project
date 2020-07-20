c6c5d94dcbd2773f460cb6f83271646f2394b930
Sam M <62692646+seneka13@users.noreply.github.com>
2020-07-20 10:48:45 +0600

    Updates

---
+++ precache-manifest.4205517bb452341800d6c659c723b50e.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5156b997a3918a1174546d8ee70872b6",
+    "url": "/ShareYSelf-project/index.html"
+  },
+  {
+    "revision": "70f46b7c3f4191f221fc",
+    "url": "/ShareYSelf-project/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b6481f86ee9c5e966e23",
+    "url": "/ShareYSelf-project/static/css/main.e4dc16e8.chunk.css"
+  },
+  {
+    "revision": "70f46b7c3f4191f221fc",
+    "url": "/ShareYSelf-project/static/js/2.8e0df64e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ShareYSelf-project/static/js/2.8e0df64e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6481f86ee9c5e966e23",
+    "url": "/ShareYSelf-project/static/js/main.8e16f9da.chunk.js"
+  },
+  {
+    "revision": "4dfd757c77ddf704a61c",
+    "url": "/ShareYSelf-project/static/js/runtime-main.05719476.js"
+  },
+  {
+    "revision": "c25a27ec43b7428120491605367eb36e",
+    "url": "/ShareYSelf-project/static/media/aboutBg.c25a27ec.jpg"
+  },
+  {
+    "revision": "975cbcf4a56b9423e79445ce2e721825",
+    "url": "/ShareYSelf-project/static/media/bg2.975cbcf4.jpg"
+  },
+  {
+    "revision": "310ddd603d15bda90e45960b4e120f8e",
+    "url": "/ShareYSelf-project/static/media/bg3.310ddd60.jpg"
+  },
+  {
+    "revision": "5cd5996ca4eea6c199d806145c367d1a",
+    "url": "/ShareYSelf-project/static/media/eBg.5cd5996c.jpg"
+  },
+  {
+    "revision": "701243cc4330386949a7b84841e307bd",
+    "url": "/ShareYSelf-project/static/media/event.701243cc.png"
+  },
+  {
+    "revision": "dcc19bac12b9f39a44c441f3e25ccd27",
+    "url": "/ShareYSelf-project/static/media/hBg1.dcc19bac.jpg"
+  },
+  {
+    "revision": "bcb681c8a3af98bdb77dc748f66f365a",
+    "url": "/ShareYSelf-project/static/media/hBg2.bcb681c8.jpg"
+  },
+  {
+    "revision": "05d0d75578439f5c992d7f8408ff9dbe",
+    "url": "/ShareYSelf-project/static/media/hBg3.05d0d755.jpg"
+  },
+  {
+    "revision": "f06661d8902d7eea01263d7ce7ee1e6d",
+    "url": "/ShareYSelf-project/static/media/meetup.f06661d8.png"
+  },
+  {
+    "revision": "b5174d8ce42c5e76e7e363ac1ed6e913",
+    "url": "/ShareYSelf-project/static/media/userBg.b5174d8c.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.20121ee6e136cc68d19da1b4dc1a3ffd.js"
+  "/ShareYSelf-project/precache-manifest.4205517bb452341800d6c659c723b50e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ShareYSelf-project/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
